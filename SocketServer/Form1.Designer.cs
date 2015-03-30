81893773d6cd9dea5eec3bdb2b65faa916944ebc
huzuohuyou <1255246777@qq.com>
2015-03-30 17:49:54 +0800

    1.0.0.1
    socket服务端

---
+++ SocketServer/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace SocketServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_ip = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_port = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rich_back = new System.Windows.Forms.RichTextBox();
+            this.txt_message = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dgv_client = new System.Windows.Forms.DataGridView();
+            this.Client = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cb_all = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_client)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始服务器";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(188, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "关闭服务器";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ip";
+            // 
+            // txt_ip
+            // 
+            this.txt_ip.Location = new System.Drawing.Point(90, 11);
+            this.txt_ip.Name = "txt_ip";
+            this.txt_ip.Size = new System.Drawing.Size(92, 21);
+            this.txt_ip.TabIndex = 3;
+            this.txt_ip.Text = "127.0.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // txt_port
+            // 
+            this.txt_port.Location = new System.Drawing.Point(90, 40);
+            this.txt_port.Name = "txt_port";
+            this.txt_port.Size = new System.Drawing.Size(92, 21);
+            this.txt_port.TabIndex = 3;
+            this.txt_port.Text = "3000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "客户端信息";
+            // 
+            // rich_back
+            // 
+            this.rich_back.Location = new System.Drawing.Point(12, 92);
+            this.rich_back.Name = "rich_back";
+            this.rich_back.Size = new System.Drawing.Size(256, 188);
+            this.rich_back.TabIndex = 5;
+            this.rich_back.Text = "";
+            // 
+            // txt_message
+            // 
+            this.txt_message.Location = new System.Drawing.Point(12, 287);
+            this.txt_message.Name = "txt_message";
+            this.txt_message.Size = new System.Drawing.Size(161, 21);
+            this.txt_message.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(188, 286);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "发送消息";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_message);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.rich_back);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_ip);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_port);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.cb_all);
+            this.splitContainer1.Panel2.Controls.Add(this.dgv_client);
+            this.splitContainer1.Size = new System.Drawing.Size(518, 318);
+            this.splitContainer1.SplitterDistance = 283;
+            this.splitContainer1.TabIndex = 9;
+            // 
+            // dgv_client
+            // 
+            this.dgv_client.AllowUserToAddRows = false;
+            this.dgv_client.AllowUserToDeleteRows = false;
+            this.dgv_client.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_client.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Client});
+            this.dgv_client.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_client.Location = new System.Drawing.Point(0, 0);
+            this.dgv_client.Name = "dgv_client";
+            this.dgv_client.ReadOnly = true;
+            this.dgv_client.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgv_client.RowHeadersWidth = 10;
+            this.dgv_client.RowTemplate.Height = 23;
+            this.dgv_client.Size = new System.Drawing.Size(231, 318);
+            this.dgv_client.TabIndex = 0;
+            this.dgv_client.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_client_CellClick);
+            // 
+            // Client
+            // 
+            this.Client.DataPropertyName = "Client";
+            this.Client.HeaderText = "客户端";
+            this.Client.MinimumWidth = 200;
+            this.Client.Name = "Client";
+            this.Client.ReadOnly = true;
+            this.Client.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Client.Width = 200;
+            // 
+            // cb_all
+            // 
+            this.cb_all.AutoSize = true;
+            this.cb_all.Location = new System.Drawing.Point(149, 3);
+            this.cb_all.Name = "cb_all";
+            this.cb_all.Size = new System.Drawing.Size(60, 16);
+            this.cb_all.TabIndex = 10;
+            this.cb_all.Text = "所有人";
+            this.cb_all.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 318);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_client)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_ip;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_port;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rich_back;
+        private System.Windows.Forms.TextBox txt_message;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dgv_client;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Client;
+        private System.Windows.Forms.CheckBox cb_all;
+
+    }
+}
+
---
+++ SocketServer/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace SocketServer
+{
+    public partial class Form1 : Form
+    {
+        Socket ss = null;
+        Socket s = null;
+        public static List<Socket> sl = new List<Socket>();
+        public static List<String> nl = new List<String>();
+
+        Thread clientThread = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int port = int.Parse(txt_port.Text);
+                string host = txt_ip.Text;
+                //创建终结点
+                IPAddress ip = IPAddress.Parse(host);
+                IPEndPoint ipe = new IPEndPoint(ip, port);
+                //创建Socket并开始监听
+                s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); //创建一个Socket对象，如果用UDP协议，则要用SocketTyype.Dgram类型的套接字
+                s.Bind(ipe);    //绑定EndPoint对象(2000端口和ip地址)
+                s.Listen(0);    //开始监听
+                //为新建立的连接创建新的Socket
+                Thread c = new Thread(new ThreadStart(AcceptClient));
+                c.Start();
+                SetText("开始监听");
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        public void RefreshClient()
+        {
+            DataTable _dtSocket = new DataTable();
+            _dtSocket.Columns.Add("Client");
+            foreach (string item in nl)
+            {
+                _dtSocket.Rows.Add(item);
+            }
+            SetDataSource(_dtSocket);
+        }
+
+        public void AcceptClient()
+        {
+            try
+            {
+                while (true)
+                {
+                    ss = s.Accept();
+                    sl.Add(ss);
+                    nl.Add(Dns.GetHostName());
+                    RefreshClient();
+                    clientThread = new Thread(new ThreadStart(ReceiveData));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.ToString());
+            }
+        }
+
+        public delegate void SetDataSourceHandler(DataTable p_dtSource);
+        private void SetDataSource(DataTable p_dtSource)
+        {
+            if (dgv_client.InvokeRequired == true)
+            {
+                SetDataSourceHandler set = new SetDataSourceHandler(SetDataSource);//委托的方法参数应和SetText一致 
+                dgv_client.Invoke(set, new object[] { p_dtSource }); //此方法第二参数用于传入方法,代替形参text 
+            }
+            else
+            {
+                dgv_client.DataSource = p_dtSource;
+            }
+
+        }
+
+        public delegate void SetTextHandler(string text);
+        private void SetText(string text)
+        {
+            if (rich_back.InvokeRequired == true)
+            {
+                SetTextHandler set = new SetTextHandler(SetText);//委托的方法参数应和SetText一致 
+                rich_back.Invoke(set, new object[] { text }); //此方法第二参数用于传入方法,代替形参text 
+            }
+            else
+            {
+                rich_back.Text += text;
+            }
+
+        } 
+
+
+        public void ReceiveData()
+        {
+            try
+            {
+                while (true)
+                {
+                    string recvStr = "";
+                    byte[] recvBytes = new byte[1024];
+                    int bytes;
+                    bytes = ss.Receive(recvBytes, recvBytes.Length, 0); //从客户端接受消息
+                    recvStr = "\n Client:" + Encoding.ASCII.GetString(recvBytes, 0, bytes);
+                    SetText(recvStr);
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //foreach (Socket item in sl)
+            //{
+            //    item.Close();
+            //}
+            if (ss != null)
+            {
+                ss.Close();
+            }
+            if (s != null)
+            {
+                s.Close();
+            }
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //给Client端返回信息
+            string sendStr = txt_message.Text;
+            if (cb_all.Checked)
+            {
+                SendAllMessage(sendStr);
+            }
+            else
+            {
+                SendOneMessage(sendStr);
+            }
+            rich_back.Text += "\n 我：" + sendStr;
+            txt_message.Text = "";
+        }
+
+        public void SendAllMessage(string p_strsend)
+        {
+            foreach (Socket item in sl)
+            {
+                byte[] bs = Encoding.ASCII.GetBytes(p_strsend);
+                ss.Send(bs, bs.Length, 0);  //返回信息给客户端
+            }
+        }
+
+        public void SendOneMessage(string p_strsend)
+        {
+            byte[] bs = Encoding.ASCII.GetBytes(p_strsend);
+            ss.Send(bs, bs.Length, 0);  //返回信息给客户端
+        }
+
+
+        private void dgv_client_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ss = sl[e.RowIndex];
+        }
+
+    }
+}
